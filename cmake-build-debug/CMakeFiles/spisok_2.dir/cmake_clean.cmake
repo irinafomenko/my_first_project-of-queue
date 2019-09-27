afa939811de5de2f407346182ba1464815638754
Fomenko Irina <irina.fomenko@mera.com>
2019-09-27 16:22:05 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/spisok_2.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spisok_2.dir/main.cpp.obj"
+  "CMakeFiles/spisok_2.dir/myqueue.cpp.obj"
+  "CMakeFiles/spisok_2.dir/newqueue.cpp.obj"
+  "spisok_2.pdb"
+  "spisok_2.exe"
+  "spisok_2.exe.manifest"
+  "libspisok_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spisok_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
